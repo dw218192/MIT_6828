d9a6099d00fe477928aecc55b7d77d5521987fcf
Tongwei Dai <tongweid@usc.edu>
2021-04-25 05:42:44 -0700

    lab3 starting point, merged with lab2

---
+++ lab3/jos/inc/x86.h
@@ -0,0 +1,337 @@
+#ifndef JOS_INC_X86_H
+#define JOS_INC_X86_H
+
+#include <inc/types.h>
+
+enum {
+  CPUID_GETVENDORSTRING,
+  CPUID_GETFEATURES,
+  CPUID_GETTLB,
+  CPUID_GETSERIAL,
+ 
+  CPUID_INTELEXTENDED = 0x80000000,
+  CPUID_INTELFEATURES,
+  CPUID_INTELBRANDSTRING,
+  CPUID_INTELBRANDSTRINGMORE,
+  CPUID_INTELBRANDSTRINGEND,
+};
+
+enum {
+    CPUID_FEAT_ECX_SSE3         = 1 << 0, 
+    CPUID_FEAT_ECX_PCLMUL       = 1 << 1,
+    CPUID_FEAT_ECX_DTES64       = 1 << 2,
+    CPUID_FEAT_ECX_MONITOR      = 1 << 3,  
+    CPUID_FEAT_ECX_DS_CPL       = 1 << 4,  
+    CPUID_FEAT_ECX_VMX          = 1 << 5,  
+    CPUID_FEAT_ECX_SMX          = 1 << 6,  
+    CPUID_FEAT_ECX_EST          = 1 << 7,  
+    CPUID_FEAT_ECX_TM2          = 1 << 8,  
+    CPUID_FEAT_ECX_SSSE3        = 1 << 9,  
+    CPUID_FEAT_ECX_CID          = 1 << 10,
+    CPUID_FEAT_ECX_FMA          = 1 << 12,
+    CPUID_FEAT_ECX_CX16         = 1 << 13, 
+    CPUID_FEAT_ECX_ETPRD        = 1 << 14, 
+    CPUID_FEAT_ECX_PDCM         = 1 << 15, 
+    CPUID_FEAT_ECX_PCIDE        = 1 << 17, 
+    CPUID_FEAT_ECX_DCA          = 1 << 18, 
+    CPUID_FEAT_ECX_SSE4_1       = 1 << 19, 
+    CPUID_FEAT_ECX_SSE4_2       = 1 << 20, 
+    CPUID_FEAT_ECX_x2APIC       = 1 << 21, 
+    CPUID_FEAT_ECX_MOVBE        = 1 << 22, 
+    CPUID_FEAT_ECX_POPCNT       = 1 << 23, 
+    CPUID_FEAT_ECX_AES          = 1 << 25, 
+    CPUID_FEAT_ECX_XSAVE        = 1 << 26, 
+    CPUID_FEAT_ECX_OSXSAVE      = 1 << 27, 
+    CPUID_FEAT_ECX_AVX          = 1 << 28,
+ 
+    CPUID_FEAT_EDX_FPU          = 1 << 0,  
+    CPUID_FEAT_EDX_VME          = 1 << 1,  
+    CPUID_FEAT_EDX_DE           = 1 << 2,  
+    CPUID_FEAT_EDX_PSE          = 1 << 3,  
+    CPUID_FEAT_EDX_TSC          = 1 << 4,  
+    CPUID_FEAT_EDX_MSR          = 1 << 5,  
+    CPUID_FEAT_EDX_PAE          = 1 << 6,  
+    CPUID_FEAT_EDX_MCE          = 1 << 7,  
+    CPUID_FEAT_EDX_CX8          = 1 << 8,  
+    CPUID_FEAT_EDX_APIC         = 1 << 9,  
+    CPUID_FEAT_EDX_SEP          = 1 << 11, 
+    CPUID_FEAT_EDX_MTRR         = 1 << 12, 
+    CPUID_FEAT_EDX_PGE          = 1 << 13, 
+    CPUID_FEAT_EDX_MCA          = 1 << 14, 
+    CPUID_FEAT_EDX_CMOV         = 1 << 15, 
+    CPUID_FEAT_EDX_PAT          = 1 << 16, 
+    CPUID_FEAT_EDX_PSE36        = 1 << 17, 
+    CPUID_FEAT_EDX_PSN          = 1 << 18, 
+    CPUID_FEAT_EDX_CLF          = 1 << 19, 
+    CPUID_FEAT_EDX_DTES         = 1 << 21, 
+    CPUID_FEAT_EDX_ACPI         = 1 << 22, 
+    CPUID_FEAT_EDX_MMX          = 1 << 23, 
+    CPUID_FEAT_EDX_FXSR         = 1 << 24, 
+    CPUID_FEAT_EDX_SSE          = 1 << 25, 
+    CPUID_FEAT_EDX_SSE2         = 1 << 26, 
+    CPUID_FEAT_EDX_SS           = 1 << 27, 
+    CPUID_FEAT_EDX_HTT          = 1 << 28, 
+    CPUID_FEAT_EDX_TM1          = 1 << 29, 
+    CPUID_FEAT_EDX_IA64         = 1 << 30,
+    CPUID_FEAT_EDX_PBE          = 1 << 31
+};
+
+static inline void
+breakpoint(void)
+{
+	asm volatile("int3");
+}
+
+static inline uint8_t
+inb(int port)
+{
+	uint8_t data;
+	asm volatile("inb %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+}
+
+static inline void
+insb(int port, void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\tinsb"
+		     : "=D" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "memory", "cc");
+}
+
+static inline uint16_t
+inw(int port)
+{
+	uint16_t data;
+	asm volatile("inw %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+}
+
+static inline void
+insw(int port, void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\tinsw"
+		     : "=D" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "memory", "cc");
+}
+
+static inline uint32_t
+inl(int port)
+{
+	uint32_t data;
+	asm volatile("inl %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+}
+
+static inline void
+insl(int port, void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\tinsl"
+		     : "=D" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "memory", "cc");
+}
+
+static inline void
+outb(int port, uint8_t data)
+{
+	asm volatile("outb %0,%w1" : : "a" (data), "d" (port));
+}
+
+static inline void
+outsb(int port, const void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\toutsb"
+		     : "=S" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "cc");
+}
+
+static inline void
+outw(int port, uint16_t data)
+{
+	asm volatile("outw %0,%w1" : : "a" (data), "d" (port));
+}
+
+static inline void
+outsw(int port, const void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\toutsw"
+		     : "=S" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "cc");
+}
+
+static inline void
+outsl(int port, const void *addr, int cnt)
+{
+	asm volatile("cld\n\trepne\n\toutsl"
+		     : "=S" (addr), "=c" (cnt)
+		     : "d" (port), "0" (addr), "1" (cnt)
+		     : "cc");
+}
+
+static inline void
+outl(int port, uint32_t data)
+{
+	asm volatile("outl %0,%w1" : : "a" (data), "d" (port));
+}
+
+static inline void
+invlpg(void *addr)
+{
+	asm volatile("invlpg (%0)" : : "r" (addr) : "memory");
+}
+
+static inline void
+lidt(void *p)
+{
+	asm volatile("lidt (%0)" : : "r" (p));
+}
+
+static inline void
+lgdt(void *p)
+{
+	asm volatile("lgdt (%0)" : : "r" (p));
+}
+
+static inline void
+lldt(uint16_t sel)
+{
+	asm volatile("lldt %0" : : "r" (sel));
+}
+
+static inline void
+ltr(uint16_t sel)
+{
+	asm volatile("ltr %0" : : "r" (sel));
+}
+
+static inline void
+lcr0(uint32_t val)
+{
+	asm volatile("movl %0,%%cr0" : : "r" (val));
+}
+
+static inline uint32_t
+rcr0(void)
+{
+	uint32_t val;
+	asm volatile("movl %%cr0,%0" : "=r" (val));
+	return val;
+}
+
+static inline uint32_t
+rcr2(void)
+{
+	uint32_t val;
+	asm volatile("movl %%cr2,%0" : "=r" (val));
+	return val;
+}
+
+static inline void
+lcr3(uint32_t val)
+{
+	asm volatile("movl %0,%%cr3" : : "r" (val));
+}
+
+static inline uint32_t
+rcr3(void)
+{
+	uint32_t val;
+	asm volatile("movl %%cr3,%0" : "=r" (val));
+	return val;
+}
+
+static inline void
+lcr4(uint32_t val)
+{
+	asm volatile("movl %0,%%cr4" : : "r" (val));
+}
+
+static inline uint32_t
+rcr4(void)
+{
+	uint32_t cr4;
+	asm volatile("movl %%cr4,%0" : "=r" (cr4));
+	return cr4;
+}
+
+static inline void
+tlbflush(void)
+{
+	uint32_t cr3;
+	asm volatile("movl %%cr3,%0" : "=r" (cr3));
+	asm volatile("movl %0,%%cr3" : : "r" (cr3));
+}
+
+static inline uint32_t
+read_eflags(void)
+{
+	uint32_t eflags;
+	asm volatile("pushfl; popl %0" : "=r" (eflags));
+	return eflags;
+}
+
+static inline void
+write_eflags(uint32_t eflags)
+{
+	asm volatile("pushl %0; popfl" : : "r" (eflags));
+}
+
+static inline uint32_t
+read_ebp(void)
+{
+	uint32_t ebp;
+	asm volatile("movl %%ebp,%0" : "=r" (ebp));
+	return ebp;
+}
+
+static inline uint32_t
+read_esp(void)
+{
+	uint32_t esp;
+	asm volatile("movl %%esp,%0" : "=r" (esp));
+	return esp;
+}
+
+static inline void
+cpuid(uint32_t info, uint32_t *eaxp, uint32_t *ebxp, uint32_t *ecxp, uint32_t *edxp)
+{
+	uint32_t eax, ebx, ecx, edx;
+	asm volatile("cpuid"
+		     : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+		     : "a" (info));
+	if (eaxp)
+		*eaxp = eax;
+	if (ebxp)
+		*ebxp = ebx;
+	if (ecxp)
+		*ecxp = ecx;
+	if (edxp)
+		*edxp = edx;
+}
+
+static inline uint64_t
+read_tsc(void)
+{
+	uint64_t tsc;
+	asm volatile("rdtsc" : "=A" (tsc));
+	return tsc;
+}
+
+static inline uint32_t
+xchg(volatile uint32_t *addr, uint32_t newval)
+{
+	uint32_t result;
+
+	// The + in "+m" denotes a read-modify-write operand.
+	asm volatile("lock; xchgl %0, %1"
+		     : "+m" (*addr), "=a" (result)
+		     : "1" (newval)
+		     : "cc");
+	return result;
+}
+
+#endif /* !JOS_INC_X86_H */
